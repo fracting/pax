c3094d5914c4142d43142ff360a552c3878427d1
Thorsten Glaser <tg@mirbsd.org>
2009-11-22 22:15:35 +0000

    since FreeWRT still uses Suckwürstchen, which hasn’t managed in how-many-years to get RCS IDs, the master copies of these files shall henceforth be in the MirCVS Repository

--- debian/files/fgetln.c
+++ debian/files/fgetln.c
@@ -1,53 +1,5 @@
-/* $FreeWRT: contrib/code/mirmake/dist/contrib/fgetln.c,v 1.5 2009/05/20 10:40:28 tg Exp $ */
+$MirOS: pax/debian/files/fgetln.c,v 1.2 2009/11/22 22:15:35 tg Exp $
 
-/*-
- * Copyright (c) 2007
- *	Thorsten Glaser <tg@mirbsd.de>
- *
- * Provided that these terms and disclaimer and all copyright notices
- * are retained or reproduced in an accompanying document, permission
- * is granted to deal in this work without restriction, including un-
- * limited rights to use, publicly perform, distribute, sell, modify,
- * merge, give away, or sublicence.
- *
- * This work is provided "AS IS" and WITHOUT WARRANTY of any kind, to
- * the utmost extent permitted by applicable law, neither express nor
- * implied; without malicious intent or gross negligence. In no event
- * may a licensor, author or contributor be held liable for indirect,
- * direct, other damage, loss, or other issues arising in any way out
- * of dealing in the work, even if advised of the possibility of such
- * damage or existence of a defect, except proven that it results out
- * of said person's immediate fault when using the work as intended.
- *-
- * fgetln() wrapper for operating systems with getline() – glibc
- */
+placeholder: pull from ↓ instead!
 
-#define _GNU_SOURCE		/* for getline() */
-#include <sys/types.h>
-#include <stdio.h>
-#include <string.h>
-
-__RCSID("$MirOS: pax/debian/files/fgetln.c,v 1.1 2009/11/22 22:00:11 tg Exp $");
-
-#if defined(__GLIBC__)
-
-#if !defined(_MIRMAKE_H) || !defined(_MIRMAKE_DEFNS)
-char *fgetln(FILE *, size_t *);
-#endif
-
-char *
-fgetln(FILE *stream, size_t *len)
-{
-	static char *lb = NULL;
-	static size_t lbsz = 0;
-
-	if ((*len = getline(&lb, &lbsz, stream)) != (size_t)-1)
-		return (lb);
-
-	/* not required by manpage, but reference implementation does this */
-	*len = 0;
-
-	/* not required to zero lb or lbsz: getdelim manages it */
-	return (NULL);
-}
-#endif
+mircvs://contrib/code/mirmake/dist/contrib/fgetln.c
