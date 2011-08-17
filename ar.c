72def23cbb8d27eabbc58e8dfea63465aef7650a
Thorsten Glaser <tg@mirbsd.org>
2011-08-17 10:54:01 +0000

    The distfile is called mircpio_20110817.orig.tar.gz though…

--- ar.c
+++ ar.c
@@ -40,7 +40,7 @@
 #include "options.h"
 #include "ar.h"
 
-__RCSID("$MirOS: src/bin/pax/ar.c,v 1.3 2011/08/17 09:46:20 tg Exp $");
+__RCSID("$MirOS: src/bin/pax/ar.c,v 1.5 2011/08/17 10:44:08 tg Exp $");
 
 /*
  * Routines for reading and writing Unix Archiver format libraries
@@ -241,6 +241,8 @@ uar_wr(ARCHD *arcn)
 	/* find out basename */
 	if ((extname = strrchr(arcn->name, '/')) == NULL)
 		extname = arcn->name;
+	else
+		++extname;
 
 	t_uid = (anonarch & ANON_UIDGID) ? 0UL : (u_long)arcn->sb.st_uid;
 	t_gid = (anonarch & ANON_UIDGID) ? 0UL : (u_long)arcn->sb.st_gid;
@@ -300,7 +302,7 @@ uar_wr(ARCHD *arcn)
 	    (extname ? strlen(extname) : 0));
 	h.ar_magic[0] = 0x60;
 	h.ar_magic[1] = 0x0A;
-	arcn->pad = arcn->sb.st_size & 1;
+	arcn->pad = (arcn->sb.st_size + (extname ? strlen(extname) : 0)) & 1;
 
 	if (wr_rdbuf((void *)&h, sizeof(HD_AR)) < 0)
 		return (-1);
